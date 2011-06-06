58c07817d0809b8d99566e978190dcccd485f907
Arun Dutta <adutta13@gmail.com>
2011-06-06 14:01:03 -0700

    Added tables

--- jscript.js
+++ jscript.js
@@ -2,7 +2,9 @@ $.get(
 	"http://jsonp.deployfu.com/clean/http%3A%2F%2Fromshare.deployfu.com%2Fmanifest",
 	function(data) {
 		$.each(data.manifests,function(i, val){
-			$('.it').append('<a href=' + val.manifest + '>' + val.id + '</a>\n<br>');
+			$('.it').append('<tr class= tx' + i + '><td><a href=' + val.manifest + '>' + val.id + '</a></td><td>');
+			$('.tx' + i).append('<td>'+ val.developer + '</td></tr>');
+			$('.tx'+i).append('<td>'+ val.summary + '</td></tr>');
 		});
 	},
 	"jsonp"

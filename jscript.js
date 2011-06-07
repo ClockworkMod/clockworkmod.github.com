448bd308fe30998e2fed66021d578439e971c680
Arun Dutta <adutta13@gmail.com>
2011-06-06 22:10:51 -0700

    Fixed row errors.

--- jscript.js
+++ jscript.js
@@ -2,8 +2,8 @@ $.get(
 	"http://jsonp.deployfu.com/clean/http%3A%2F%2Fromshare.deployfu.com%2Fmanifest",
 	function(data) {
 		$.each(data.manifests,function(i, val){
-			$('.it').append('<tr class= tx' + i + '><td><a href=' + val.manifest + '>' + val.id + '</a></td><td>');
-			$('.tx' + i).append('<td>'+ val.developer + '</td></tr>');
+			$('.it').append('<tr class= tx' + i + '><td><a href=' + val.manifest + '>' + val.id + '</a></td>');
+			$('.tx' + i).append('<td>'+ val.developer + '</td>');
 			$('.tx'+i).append('<td class = "summary">'+ val.summary + '</td></tr>');
 		});
 	},

4f9eeafeab9a30f6d41fbc83d41c40653f02cb21
Arun Dutta <adutta13@gmail.com>
2011-06-07 18:05:44 -0700

    added style sheet, rom listing, and icons

--- jscript.js
+++ jscript.js
@@ -1,35 +1,38 @@
-$(document).ready(function(){
-	$("a.hide").hide();
-	$('td.summary').hide();
-});
+var manArr = new Array();
 
 $.get(
-	"http://jsonp.deployfu.com/clean/http%3A%2F%2Fromshare.deployfu.com%2Fmanifest",
-	function(data) {
-		$.each(data.manifests,function(i, val){
-			$('.it').append('<tr class= tx' + i + '><td><a href=' + val.manifest + '>' + val.id + '</a></td>');
-			$('.tx' + i).append('<td>'+ val.developer + '</td>');
-			$('.tx'+i).append('<td class = "summary">'+ val.summary + '</td></tr>');
-		});
-		$('td.summary').hide();
-	},
-	"jsonp"
-);
+"http://jsonp.deployfu.com/clean/http%3A%2F%2Fromshare.deployfu.com%2Fmanifest",
+function(data) {
 
-$(document).ready(function(){
-	
-	$("a.show").click(function(event){
-		event.preventDefault();
-		$("a.show").hide();
-		$("a.hide").show();
-		$("td.summary").show();
-	});
-	
-	$("a.hide").click(function(event){
-		event.preventDefault();
-		$("a.show").show();
-		$("a.hide").hide();
-		$("td.summary").hide();
-	});
-	
-});
+
+    // Display the developer, his ID, and  the summary
+    // Get manifests into an array
+    $.each(data.manifests,
+    function(i, val) {
+        $('.it').append('<tr class= tx' + i + '><td class = "ty' + i + '><a href="" class = "ax' + i + '">' + val.developer + '</a><br><img src="' + val.icon + '" width = "100" height ="100" alt = "' + val.id + '"></td>');
+        $('.tx' + i).append('<td class= "summary"><b>ROMS:</b><ol class = ox'+i+'></ol><br><br> '+ val.summary +' </td></tr>');
+
+        manArr.unshift(val.manifest);
+    });
+
+
+    //Display latest 3 roms for each developer
+    $.each(manArr, function(i, val){
+    	$.get(
+    		"http://jsonp.deployfu.com/clean/" + encodeURIComponent(val),
+    		function(data){
+				for(j=0;j<data.roms.length;j++){
+					$('ol.ox' + i).append('<li>' + data.roms[j].name + '</li></tr>');
+					if (j>3) {
+						j=0;
+						break;
+					}
+				}
+    			
+    		},
+    		"jsonp"
+    	);
+    });
+},
+"jsonp"
+);
